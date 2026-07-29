ba6d69d3eeaf088e3a04c8651069141e18b260f4
Ekan <ekanvicky@gmail.com>
2026-07-29 16:59:51 +0100

    Worked on portfolio's page responsiveness

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -13,7 +13,7 @@ const Portfolio = ({ ref }) => {
       <div className="w-full flex flex-col gap-y-20 mt-2 px-12 ">
         <div className="flex flex-col justify-center relative px-6 bg-[var(--bg2)] h-52 border-1 border-zinc-300 rounded-2xl backdrop-blur-sm cursor-pointer hover:shadow-lg hover:scale-103">
           <div className="flex flex-col gap-12">
-            <h2 className="text-left text-zinc-100 text-3xl font-makira">
+            <h2 className="text-left text-zinc-100 text-[1.75rem] font-makira">
               Let's talk<span className="block">about your project</span>
             </h2>
             <button className="w-fit text-left px-10 py-3 bg-zinc-100 text-zinc-800 text-xl rounded-3xl uppercase cursor-pointer">
@@ -21,40 +21,40 @@ const Portfolio = ({ ref }) => {
             </button>
           </div>
         </div>
-        <div className="flex flex-col justify-center relative px-6 py-12 bg-zinc-300/20 h-52 border-1 border-zinc-300 rounded-2xl backdrop-blur-sm cursor-pointer hover:shadow-lg hover:scale-103">
-          <div className="absolute flex flex-col items-center justify-center bg-white border-1 border-zinc-400 bg-zinc-100 w-[60px] h-[60px] rounded-[50%] top-[-20px] right-[-20px]">
+        <div className="flex flex-col justify-center relative px-6 py-[9em] bg-zinc-300/20 h-52 border-1 border-zinc-300 rounded-2xl backdrop-blur-sm cursor-pointer hover:shadow-lg hover:scale-103">
+          <div className="absolute flex flex-col items-center justify-center bg-white border-1 border-zinc-400 bg-zinc-100 w-[55px] h-[55px] rounded-[50%] top-[-20px] right-[-20px]">
             <img
               className="contrast-1"
               src={reactIcon}
               alt="React icon"
-              width={50}
+              width={45}
             ></img>
           </div>
           <div className="flex flex-col items-start gap-8">
             <h2 className="text-left text-zinc-800 text-3xl font-makira">
               Frontend Dev.
             </h2>
-            <p className="text-left">
+            <p className="text-left w-60">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Explicabo accusamus cumque voluptas ab veniam, doloremque rerum
               ratione, sint iure tenetur.
             </p>
           </div>
         </div>
-        <div className="flex flex-col justify-center relative px-6 py-12 bg-zinc-300/20 h-52 border-1 border-zinc-300 rounded-2xl backdrop-blur-sm cursor-pointer hover:shadow-lg hover:scale-103">
-          <div className="absolute flex flex-col items-center justify-center bg-white border-1 border-zinc-400 w-[60px] h-[60px] rounded-[50%] top-[-20px] right-[-20px]">
+        <div className="flex flex-col justify-center relative px-6 py-[9em] bg-zinc-300/20 h-52 border-1 border-zinc-300 rounded-2xl backdrop-blur-sm cursor-pointer hover:shadow-lg hover:scale-103">
+          <div className="absolute flex flex-col items-center justify-center bg-white border-1 border-zinc-400 w-[55px] h-[55px] rounded-[50%] top-[-20px] right-[-20px]">
             <img
               className="contrast-1"
               src={backendIcon}
               alt="React icon"
-              width={42}
+              width={38}
             ></img>
           </div>
           <div className="flex flex-col gap-8">
             <h2 className="text-left text-zinc-800 text-3xl font-makira">
-              Backend Engineer
+              Backend Eng.
             </h2>
-            <p className="text-left">
+            <p className="text-left w-60">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Explicabo accusamus cumque voluptas ab veniam, doloremque rerum
               ratione, sint iure tenetur.
